72a07e4860f98f2e51834c139953bfbc69060b08
hebeshen <hebeshen@byu.edu>
2025-07-30 06:57:48 -0600

    commands and message

--- shared/src/main/java/websocket/commands/ConnectCommand.java
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -1,4 +1,21 @@
 package websocket.commands;
+import chess.ChessGame.TeamColor;
 
-public class ConnectCommand {
-}
+public class ConnectCommand extends UserGameCommand {
+    private final String username;
+    private final TeamColor playerColor;
+
+    public ConnectCommand(String authToken, Integer gameID, String username, TeamColor playerColor) {
+        super(CommandType.CONNECT, authToken, gameID);
+        this.username = username;
+        this.playerColor = playerColor;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+    public TeamColor getPlayerColor() {
+        return playerColor;
+    }}
+
--- shared/src/main/java/websocket/commands/ResignCommand.java
+++ shared/src/main/java/websocket/commands/ResignCommand.java
@@ -1,4 +1,6 @@
 package websocket.commands;
 
-public class ResignCommand {
-}
+public class ResignCommand extends UserGameCommand {
+    public ResignCommand(String authToken, Integer gameID) {
+        super(CommandType.RESIGN, authToken, gameID);
+    }}
