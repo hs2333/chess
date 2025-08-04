be12a1c1c2cc9684368d1764398162129e84574c
hebeshen <hebeshen@byu.edu>
2025-08-03 20:24:05 -0600

    gameover problem

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -11,8 +11,11 @@ public interface GameDAO {
     Collection<GameData> listGames() throws DataAccessException;
 
     void updateGame(GameData game) throws DataAccessException;
-     void clear() throws DataAccessException;
+    void clear() throws DataAccessException;
 
+    //for some reason, these two gameover methods cannot be used. They have been disgarded. I created an internal variable in WebSocketHandler.
+    //but it's hard to remove them since I've updated them in other places,
+    //so just leave them here but don't call them don't!
     void setGameOver(int gameID, boolean gameOver) throws DataAccessException;
     boolean getGameOver(int gameID) throws DataAccessException;
 }
